5bbd583a62cb442c6c5e7a0368e67034d9456297
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-01-16 19:23:24 +0100

    hangman drawer (#4)
    * Added hangman
    
    * Showing hangman depending on amount of fails

---
+++ src/components/HangmanDrawer.tsx
@@ -0,0 +1,39 @@
+const HEAD = (
+    <div className="hangman-drawer__head"></div>
+)
+
+const BODY = (
+    <div className="hangman-drawer__body"></div>
+)
+
+const RIGHT_ARM = (
+    <div className="hangman-drawer__right-arm"></div>
+)
+
+const LEFT_ARM = (
+    <div className="hangman-drawer__left-arm"></div>
+)
+
+const RIGHT_LEG = (
+    <div className="hangman-drawer__right-leg"></div>
+)
+
+const LEFT_LEG = (
+    <div className="hangman-drawer__left-leg"></div>
+)
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+function HangmanDrawer() {
+	return (
+        <div className="hangman-drawer">
+            {BODY_PARTS.slice(0, 0)}
+            <div className="hangman-drawer__top"></div>
+            <div className="hangman-drawer__top-hang"></div>
+            <div className="hangman-drawer__middle"></div>
+            <div className="hangman-drawer__bottom"></div>
+        </div>
+    );
+}
+
+export default HangmanDrawer;
