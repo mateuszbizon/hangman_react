842801e7e1a73d9a85391c59ca2b064394b63462
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-01-18 15:50:08 +0100

    Added function to get random word

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import './sass/app.scss'
 import HangmanDrawer from './components/HangmanDrawer'
 import HangmanWord from './components/HangmanWord'
+import words from "./data/words.json";
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState<string>(() => getRandomWord());
 
   return (
     <>
       <div className='hangman'>
         <HangmanDrawer />
-        <HangmanWord />
+        <HangmanWord wordToGuess={wordToGuess} />
       </div>
     </>
   )
--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,9 +1,11 @@
-function HangmanWord() {
-    const testWord = "testowanie";
+type HangmanWordProps = {
+    wordToGuess: string
+}
 
+function HangmanWord({ wordToGuess }: HangmanWordProps) {
   return (
     <div className="hangman-word">
-        {testWord.split("").map((letter, index) => {
+        {wordToGuess.split("").map((letter, index) => {
             return (
                 <span className="hangman-word__letter-border">
                     <span key={index} className="hangman-word__letter hangman-word__letter--hidden">
